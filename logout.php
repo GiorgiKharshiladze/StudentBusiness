8be84c1e87de8ce9229cc0e1b56e163c202aa74b
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-06 18:17:18 +0200

    redirection 3

--- logout.php
+++ logout.php
@@ -2,6 +2,9 @@
 <?php
 session_start();
 session_destroy();
-header('Location: http://GioKhar.Com/StudentBusiness/login.php');
+echo '<script type="text/javascript">
+           window.location = "http://GioKhar.Com/StudentBusiness/login.php"
+      </script>';
+// header('Location: http://GioKhar.Com/StudentBusiness/login.php');
 exit();
 ?>
