afff86c62b97f4ede3866b8281843aa67ce8a8f3
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-05-06 18:01:36 +0200

    ionic

---
+++ ionic/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Define database connection parameters
+   $hn      = 'localhost';
+   $un      = 'id1849536_user'; // Username
+   $pwd     = '12345678'; // Password
+   $db      = 'id1849536_db'; // Database Name
+   $cs      = 'utf8';
+
+?>
